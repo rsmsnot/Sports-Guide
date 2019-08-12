67200a4eeb7a8827f096065f0015ab3698e60dc5
Steven Wing <steven.wing82@gmail.com>
2019-08-12 16:02:13 -0700

    fix gitignore

--- test.js
+++ test.js
@@ -1,10 +1,32 @@
-const matches = require('livesoccertv-parser')
+// const matches = require('livesoccertv-parser')
 
-// async (console.log(await matches('spain', 'real-madrid'))) //=> [{...}, {...}, ...]
 
-async function f1() {
-    var x = await matches('england', 'arsenal');
-    console.log(x); // 10
-  }
+// async function f1() {
+//     var x = await matches('england', 'arsenal', { timezone: 'Europe/London' })
+//     ;
+//     console.log(x); // 10
+//   }
 
-  f1()
+//   f1()
+
+
+const rp = require('request-promise');
+
+const $ = require('cheerio');
+
+const url = 'https://www.livesoccertv.com/channels/sky-sports-premier-league/';
+
+rp(url)
+  .then(function(html){
+    //success!
+    let count = $(".matchrow:not('.repeatrow')", html).length;
+    let matches = [];
+    for (let i = 0; i < count; i++) {
+        matches.push($(".matchrow:not('.repeatrow')", html))
+    }
+    // console.log($(".matchrow:not('.repeatrow')", html);
+    console.log(matches)
+  })
+  .catch(function(err){
+    //handle error
+  });
